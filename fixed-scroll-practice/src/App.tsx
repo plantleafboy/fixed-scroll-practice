ef0bc19eaa107dea0bc49d84f95a57e0815b8eeb
Alex Long <alo72@uclive.ac.nz>
2026-01-24 17:06:09 +1300

    initial commit

---
+++ fixed-scroll-practice/src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+import LenisProvider from './components/LenisProvider';
+import './App.css'
+
+function App() {
+
+  return (
+    <LenisProvider>
+
+    </LenisProvider>
+  )
+}
+
+export default App
